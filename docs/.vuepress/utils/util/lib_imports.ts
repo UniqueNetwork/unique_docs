3d50f07c56be789a26ad5004e522afed8857bdad
Alex Saft <fend25@gmail.com>
2022-04-27 13:47:23 +0300

    changed place for polyfill import

--- docs/.vuepress/utils/util/lib_imports.ts
+++ docs/.vuepress/utils/util/lib_imports.ts
@@ -1,4 +1,14 @@
-import '_utils/util/plugin_web3'
+// import '_utils/util/plugin_web3'
+import process from 'process'
+import {Buffer} from 'buffer'
+import EventEmitter from 'events'
+
+if (typeof window !== 'undefined') {
+  window.Buffer = Buffer
+  window.process = process
+  ;(window as any).EventEmitter = EventEmitter
+  ;(window as any).global = window
+}
 
 import type Web3Class from 'web3'
 
