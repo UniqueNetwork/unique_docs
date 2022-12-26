07ee084c31f33d464ae885e44eb512d4e7804da3
Alex Saft <fend25@gmail.com>
2022-12-26 17:21:41 +0300

    fix: Details component refactoring start

--- docs/.vuepress/components/Details.vue
+++ docs/.vuepress/components/Details.vue
@@ -1,44 +1,20 @@
 <template>
-  <details class="unique-details" :style="cssVars">
+  <details class="unique-details" :class="props.size">
     <summary class="unique-summary">
       <slot name="header"/>
     </summary>
     <div class="unique-details-body">
-      <slot name="body" />
+      <slot name="body"/>
     </div>
   </details>
 </template>
 
 <script setup lang="ts">
-import {computed, PropType} from 'vue'
-import {DetailsSizes, smallDetails, defaultDetails} from './types/DetailsSizes'
+import {Prop} from 'vue'
 
-const props = defineProps({
-  size: { 
-    type: String as PropType<keyof typeof DetailsSizes>, 
-    required: false, 
-  },
-})
-
-const cssVars = computed(() => {
-  switch (props.size) {
-    case 'Small': 
-      return {
-        '--fz': smallDetails.size,
-        '--p': smallDetails.padding,
-        '--h': smallDetails.height,
-        '--w': smallDetails.width,
-      }
-    case 'Default': 
-    default: 
-      return {
-        '--fz': defaultDetails.size,
-        '--h': defaultDetails.height,
-        '--p': defaultDetails.padding,
-        '--w': defaultDetails.width,
-      }
-  }
-})
+const props = defineProps<{
+  size?: Prop<DetailsSizes>
+}>()
 </script>
 
 <style lang="scss">
@@ -46,22 +22,41 @@ const cssVars = computed(() => {
   border: thin;
   border-radius: 8px !important;
   cursor: pointer;
-  padding: 10px 15px;
-  width: var(--w);
+  padding: 10px 0;
+  width: 100%;
   min-width: 320px;
+
   .unique-summary {
-    font-size: var(--fz);
-    padding: var(--p);
     background: rgba(0, 0, 0, 0.05);
     font-weight: 400;
     margin-bottom: 5px;
-    height: var(--h) !important;
-    color:  var(--c-text);
+    font-size: 24px;
+    height: 28px !important;
+    padding: 12px 0 12px 16px;
+    color: var(--c-text);
     box-shadow: 0 0 6px var(--c-custom-text-hover-opacity);
     border-radius: 8px !important;
   }
+
+  &.small {
+    width: 50%;
+
+    .unique-summary {
+      font-size: 16px;
+      height: 20px !important;
+      padding: 8px 12px;
+    }
+  }
+
   .unique-details-body {
     margin-top: 0;
   }
 }
 </style>
+
+<script lang="ts">
+export enum DetailsSizes {
+  small = 'small',
+  regular = 'regular', //dummy, uses default value
+}
+</script>
